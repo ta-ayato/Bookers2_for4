3c76c0a200d5fd1b76a41c5a65275c987bc19a5c
ta-ayato <173433821+ta-ayato@users.noreply.github.com>
2024-09-28 12:13:08 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  resources :users, only: [:show, :edit, :index, :update]
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  root to: "homes#top"
+  get "home/about" => "homes#about"
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
